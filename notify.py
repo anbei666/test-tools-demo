955c0ffe5acda9d245a4acef1d7caf63e57084de
anbei666 <anbei_mao@foxmail.com>
2026-07-18 14:14:57 +0800

    feat: 功能分支追加通知逻辑

--- notify.py
+++ notify.py
@@ -1,1 +1,2 @@
 print('🚀 [CICD] Automation test finished! Sending report to WeChat...')print('Main branch line')
+print('🚀 [CICD] Automation test finished! Sending report to WeChat...')print('Feature branch line')
