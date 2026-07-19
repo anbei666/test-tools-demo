95fe63179b4090839ad880d3f6a3af72285b8248
anbei666 <anbei_mao@foxmail.com>
2026-07-19 16:51:29 +0800

    test for jenkinsifle tokenwork8

---
+++ tests/test_2.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    print('assert add(1, 2) == 3,just add a test pr1')
+    print('test jenkins pipeline777')
+    assert 3 == 3
+    assert add(1, 2) == 3
